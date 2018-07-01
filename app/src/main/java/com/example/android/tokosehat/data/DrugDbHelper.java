055f6954427268cea2ad59491480ca5c47d8272c
Bryan Austyn Ichsan <bryanhans1401@gmail.com>
2018-07-01 13:32:00 +0700

    Modify DrugDbHelper

--- app/src/main/java/com/example/android/tokosehat/data/DrugDbHelper.java
+++ app/src/main/java/com/example/android/tokosehat/data/DrugDbHelper.java
@@ -1,4 +1,36 @@
 package com.example.android.tokosehat.data;
 
-public class DrugDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.android.tokosehat.data.DrugContract.DrugEntry;
+
+public class DrugDbHelper extends SQLiteOpenHelper {
+
+    public static final String LOG_TAG = DrugDbHelper.class.getSimpleName();
+
+    private static final String DATABASE_NAME = "shop.db";
+
+    private static final int DATABASE_VERSION = 1;
+
+    public DrugDbHelper(Context c) {
+        super(c, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String SQL_CREATE_TABLE = "CREATE TABLE " + DrugEntry.TABLE_NAME + "("
+                + DrugEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + DrugEntry.COLUMN_DRUG_NAME + " TEXT NOT NULL, "
+                + DrugEntry.COLUMN_DRUG_DISEASESAS + " TEXT NOT NULL, "
+                + DrugEntry.COLUMN_DRUG_PRICE + " INTEGER NOT NULL DEFAULT 0, "
+                + DrugEntry.COLUMN_DRUG_STATUS + " TEXT NOT NULL);";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
