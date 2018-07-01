cb6578e885ed8daf6e39472752e675dd922ba79d
Bryan Austyn Ichsan <bryanhans1401@gmail.com>
2018-07-01 12:59:26 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/android/tokosehat/data/DrugContract.java
@@ -0,0 +1,9 @@
+package com.example.android.tokosehat.data;
+
+/**
+ * Api Contract for TokoSehat app.
+ */
+
+public class DrugContract {
+
+}
---
+++ app/src/main/java/com/example/android/tokosehat/data/DrugProvider.java
@@ -0,0 +1,4 @@
+package com.example.android.tokosehat.data;
+
+public class DrugProvider {
+}
