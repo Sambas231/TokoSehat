6076a2fb355cabea8d1f86aad243dd83eabab1d4
Bryan Austyn Ichsan <bryanhans1401@gmail.com>
2018-07-04 12:30:59 +0700

    Done modifying activity_editor

--- app/src/main/java/com/example/android/tokosehat/EditorActivity.java
+++ app/src/main/java/com/example/android/tokosehat/EditorActivity.java
@@ -1,4 +1,16 @@
 package com.example.android.tokosehat;
 
-public class EditorActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditorActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+
+
 }
