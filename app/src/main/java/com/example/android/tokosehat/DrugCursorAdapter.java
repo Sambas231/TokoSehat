a0d24e3ff820b699e5256b134d7f1fcf69a4e634
Bryan Austyn Ichsan <bryanhans1401@gmail.com>
2018-07-09 17:44:57 +0700

    Updated bindView

--- app/src/main/java/com/example/android/tokosehat/DrugCursorAdapter.java
+++ app/src/main/java/com/example/android/tokosehat/DrugCursorAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
 
 import com.example.android.tokosehat.data.DrugContract.DrugEntry;
 
@@ -23,5 +24,24 @@ public class DrugCursorAdapter extends CursorAdapter {
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        TextView nameView = (TextView) view.findViewById(R.id.list_name);
+        TextView statusView = (TextView) view.findViewById(R.id.list_status);
+        TextView priceView = (TextView) view.findViewById(R.id.list_price);
+        TextView diseasesView = (TextView) view.findViewById(R.id.list_diseases);
+
+        int nameIndex = cursor.getColumnIndex(DrugEntry.COLUMN_DRUG_NAME);
+        int statusIndex = cursor.getColumnIndex(DrugEntry.COLUMN_DRUG_STATUS);
+        int priceIndex = cursor.getColumnIndex(DrugEntry.COLUMN_DRUG_PRICE);
+        int diseasesIndex = cursor.getColumnIndex(DrugEntry.COLUMN_DRUG_DISEASES);
+
+        String name = cursor.getString(nameIndex);
+        String status = cursor.getString(statusIndex);
+        int price = cursor.getInt(priceIndex);
+        String diseases = cursor.getString(diseasesIndex);
+
+        nameView.setText(name);
+        statusView.setText(status);
+        priceView.setText(String.valueOf(price));
+        diseasesView.setText(diseases);
     }
 }
