9db9e7fa4a886a83723b3400df260b1aab94113b
Bryan Austyn Ichsan <bryanhans1401@gmail.com>
2018-07-09 20:24:24 +0700

    make saveDrug method in EditorActivity

--- app/src/main/java/com/example/android/tokosehat/EditorActivity.java
+++ app/src/main/java/com/example/android/tokosehat/EditorActivity.java
@@ -1,12 +1,14 @@
 package com.example.android.tokosehat;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
@@ -14,17 +16,19 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
 import com.example.android.tokosehat.data.DrugContract.DrugEntry;
 
 import com.example.android.tokosehat.data.DrugContract;
 
 public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
-    private boolean mPetHasChanged = false;
+    private boolean mDrugHasChanged = false;
     private View.OnTouchListener onTouchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
-            mPetHasChanged = true;
+            mDrugHasChanged = true;
             return false;
         }
     };
@@ -36,7 +40,7 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
 
     private String mStatus = DrugEntry.STATUS_OUT_OF_STOCK;
 
-    private static final int PET_LOADER = 0;
+    private static final int DRUG_LOADER = 0;
     private Uri mCurrentUri;
 
     @Override
@@ -46,10 +50,18 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
 
         mCurrentUri = getIntent().getData();
 
+        getLoaderManager().initLoader(DRUG_LOADER, null, this);
+
         mName = (EditText) findViewById(R.id.editor_edit_name);
         mDiseases = (EditText) findViewById(R.id.editor_edit_diseases);
         mPrice = (EditText) findViewById(R.id.editor_price_edit_text);
         mStatusSpinner = (Spinner) findViewById(R.id.editor_spinner);
+
+        mName.setOnTouchListener(onTouchListener);
+        mDiseases.setOnTouchListener(onTouchListener);
+        mPrice.setOnTouchListener(onTouchListener);
+        mStatusSpinner.setOnTouchListener(onTouchListener);
+
 
         setupSpinner();
     }
@@ -76,6 +88,50 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
             }
 
         });
+    }
+
+    private void savePet() {
+        String nameString = mName.getText().toString().trim();
+        String diseasesString = mDiseases.getText().toString().trim();
+        String priceString = mPrice.getText().toString().trim();
+
+        if (mCurrentUri == null && nameString.isEmpty() && diseasesString.isEmpty() &&
+                priceString.isEmpty() && mStatus == DrugEntry.STATUS_OUT_OF_STOCK) {
+            return;
+        }
+
+        ContentValues values = new ContentValues();
+        values.put(DrugEntry.COLUMN_DRUG_NAME, nameString);
+        values.put(DrugEntry.COLUMN_DRUG_DISEASES, diseasesString);
+        values.put(DrugEntry.COLUMN_DRUG_STATUS, mStatus);
+
+        int price = 0;
+        if (!priceString.isEmpty()) {
+            price = Integer.parseInt(priceString);
+        }
+        values.put(DrugEntry.COLUMN_DRUG_PRICE, price);
+
+        if (mCurrentUri == null) {
+            Uri newUri = getContentResolver().insert(DrugEntry.CONTENT_URI, values);
+
+            if (newUri == null) {
+                Toast.makeText(EditorActivity.this, "Insert drug failed", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(EditorActivity.this, "Insert drug success", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
+            int rowsAffected = getContentResolver().update(mCurrentUri, values, null, null);
+
+            if (rowsAffected == 0) {
+                Toast.makeText(EditorActivity.this, "Update drug failed", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(EditorActivity.this, "Update drug success", Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 
     @NonNull
