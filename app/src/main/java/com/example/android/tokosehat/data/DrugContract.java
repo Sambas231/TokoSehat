a4eba0c15eeb088afc499e2585eefc6a4a387506
Bryan Austyn Ichsan <bryanhans1401@gmail.com>
2018-07-12 17:07:16 +0700

    Updated DrugContract & DrugDbHelper

--- app/src/main/java/com/example/android/tokosehat/data/DrugContract.java
+++ app/src/main/java/com/example/android/tokosehat/data/DrugContract.java
@@ -37,6 +37,7 @@ public class DrugContract {
     public static final String COLUMN_DRUG_DISEASES = "diseases";
     public static final String COLUMN_DRUG_PRICE = "price";
     public static final String COLUMN_DRUG_STATUS = "status";
+    public static final String COLUMN_DRUG_DOSAGE = "dosage";
 
     public static final String STATUS_AVAILABLE = "Available";
     public static final String STATUS_OUT_OF_STOCK = "Out of stock";
--- app/src/main/java/com/example/android/tokosehat/data/DrugDbHelper.java
+++ app/src/main/java/com/example/android/tokosehat/data/DrugDbHelper.java
@@ -24,7 +24,8 @@ public class DrugDbHelper extends SQLiteOpenHelper {
                 + DrugEntry.COLUMN_DRUG_NAME + " TEXT NOT NULL, "
                 + DrugEntry.COLUMN_DRUG_DISEASES + " TEXT NOT NULL, "
                 + DrugEntry.COLUMN_DRUG_PRICE + " INTEGER NOT NULL DEFAULT 0, "
-                + DrugEntry.COLUMN_DRUG_STATUS + " TEXT NOT NULL);";
+                + DrugEntry.COLUMN_DRUG_STATUS + " TEXT NOT NULL, "
+                + DrugEntry.COLUMN_DRUG_DOSAGE + " TEXT NOT NULL);";
 
         sqLiteDatabase.execSQL(SQL_CREATE_TABLE);
     }
